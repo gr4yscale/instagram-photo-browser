ef09f2dfddcc4a7b7f7b0e383955242ee209764d
Tyler Powers <gr4yscale@gmail.com>
2013-11-05 14:49:04 -0800

    category for Photo NSManagedObject subclass to take care of import logic specific to Photo

---
+++ instagram-photo-browser/Photo+ImportAdditions.h
@@ -0,0 +1,15 @@
+//
+//  Photo+Additions.h
+//  instagram-photo-browser
+//
+//  Created by Tyler Powers on 11/4/13.
+//  Copyright (c) 2013 Tyler Powers. All rights reserved.
+//
+
+#import "Photo.h"
+
+@interface Photo (ImportAdditions)
+
++ (void)importFromDictionary:(NSDictionary *)dict intoMOC:(NSManagedObjectContext *)moc;
+
+@end
---
+++ instagram-photo-browser/Photo+ImportAdditions.m
@@ -0,0 +1,80 @@
+//
+//  Photo+Additions.m
+//  instagram-photo-browser
+//
+//  Created by Tyler Powers on 11/4/13.
+//  Copyright (c) 2013 Tyler Powers. All rights reserved.
+//
+
+#import "Photo+ImportAdditions.h"
+#import "NSDictionary+KeyValueMapping.h"
+
+#define kPhotoImportKeyIdentifier           @"identifier"
+#define kPhotoImportKeyCaption              @"caption"
+#define kPhotoImportKeyCommentCount         @"commentCount"
+#define kPhotoImportKeyFullResImageURL      @"fullResImageURL"
+#define kPhotoImportKeyLikeCount            @"likeCount"
+#define kPhotoImportKeyUserFullName         @"userFullName"
+#define kPhotoImportKeyUsername             @"username"
+#define kPhotoImportKeyUserProfilePicURL    @"userProfilePicURL"
+
+
+@implementation Photo (ImportAdditions)
+
+
+// this is slow and NOT the way to go for large imports, but for this example it should be fine.
+// will replace with Apple's recommended implementation if this becomes a performance bottleneck:
+// http://developer.apple.com/library/ios/#documentation/Cocoa/Conceptual/CoreData/Articles/cdImporting.html
+
++ (instancetype)findOrCreatePhoto:(NSString *)identifier
+                          context:(NSManagedObjectContext*)moc
+{
+    
+    NSFetchRequest* fetchRequest = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass(self)];
+    fetchRequest.fetchLimit = 1;
+    fetchRequest.predicate = [NSPredicate predicateWithFormat:@"identifier = %@", identifier];
+    
+    id object = [[moc executeFetchRequest:fetchRequest error:NULL] lastObject];
+    
+    if (!object) {
+        object = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass(self)
+                                               inManagedObjectContext:moc];
+    }
+    return object;
+}
+
+
+
++ (void)importFromDictionary:(NSDictionary *)dict intoMOC:(NSManagedObjectContext *)moc
+{
+    NSDictionary *sanitizedDict = [self sanitizedDictionaryFromDictionary:dict];
+    
+    Photo *aPhoto = [self findOrCreatePhoto:sanitizedDict[kPhotoImportKeyIdentifier]
+                                    context:moc];
+    
+    [aPhoto setValuesForKeysWithDictionary:sanitizedDict];
+}
+
+
+
++ (NSDictionary *)sanitizedDictionaryFromDictionary:(NSDictionary *)dict {
+
+    NSMutableDictionary *sanitizedDict = [NSMutableDictionary dictionary];
+
+    NSDictionary *stringsMapping = @{kPhotoImportKeyIdentifier: @"id",
+                                     kPhotoImportKeyCaption: @"caption.text",
+                                     kPhotoImportKeyFullResImageURL: @"images.standard_resolution.url",
+                                     kPhotoImportKeyUserFullName: @"user.full_name",
+                                     kPhotoImportKeyUsername: @"user.username",
+                                     kPhotoImportKeyUserProfilePicURL: @"user.profile_picture"};
+    
+    NSDictionary *numbersMapping = @{kPhotoImportKeyLikeCount: @"likes.count",
+                                     kPhotoImportKeyCommentCount: @"comments.count"};
+    
+    [NSDictionary applyMapping:stringsMapping fromDictionary:dict toDictionary:sanitizedDict forClass:[NSString class]];
+    [NSDictionary applyMapping:numbersMapping fromDictionary:dict toDictionary:sanitizedDict forClass:[NSNumber class]];
+    
+    return sanitizedDict;
+}
+
+@end
