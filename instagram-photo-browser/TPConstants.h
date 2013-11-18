ce7da197cadb68967c397844063c543f42289c90
Tyler Powers <gr4yscale@gmail.com>
2013-11-17 21:50:08 -0800

    constants updates

--- instagram-photo-browser/TPConstants.h
+++ instagram-photo-browser/TPConstants.h
@@ -8,10 +8,16 @@
 
 #define kNumberOfPhotosToDisplay                            50
 
+// ui/colors
+
 #define kTextColorPrimary                                   [UIColor colorWithRed:0 green:0 blue:0 alpha:0.85]
 #define kTextColorSecondary                                 [UIColor colorWithRed:0 green:0 blue:0 alpha:0.4]
+#define kPhotoImageViewBGColor                              [UIColor colorWithRed:230/255.0 green:230/255.0 blue:230/255.0 alpha:1.0]
 
 #define kFontHeading                                        [UIFont fontWithName:@"EuphemiaUCAS-Bold" size:14.0]
 #define kFontTitle                                          [UIFont fontWithName:@"EuphemiaUCAS" size:14.0]
 #define kFontSubtitle                                       [UIFont fontWithName:@"EuphemiaUCAS" size:12.0]
 #define kFontButtonTitle                                    [UIFont fontWithName:@"EuphemiaUCAS-Bold" size:12.0]
+
+
+#define kSpacingBetweenPhotos                               6.0
