719ef2c7219a0bc12c65c8c273c4f17f552bd11d
Tyler Powers <gr4yscale@gmail.com>
2013-11-17 22:55:46 -0800

    remove photoWidth and photoHeight from Photo managed object

--- instagram-photo-browser/Photo.h
+++ instagram-photo-browser/Photo.h
@@ -2,7 +2,7 @@
 //  Photo.h
 //  instagram-photo-browser
 //
-//  Created by Tyler Powers on 11/12/13.
+//  Created by Tyler Powers on 11/17/13.
 //  Copyright (c) 2013 Tyler Powers. All rights reserved.
 //
 
@@ -14,14 +14,12 @@
 
 @property (nonatomic, retain) NSString * caption;
 @property (nonatomic, retain) NSNumber * commentCount;
+@property (nonatomic, retain) NSDate * createdTime;
 @property (nonatomic, retain) NSString * fullResImageURL;
 @property (nonatomic, retain) NSString * identifier;
 @property (nonatomic, retain) NSNumber * likeCount;
-@property (nonatomic, retain) NSNumber * photoHeight;
-@property (nonatomic, retain) NSNumber * photoWidth;
 @property (nonatomic, retain) NSString * userFullName;
 @property (nonatomic, retain) NSString * username;
 @property (nonatomic, retain) NSString * userProfilePicURL;
-@property (nonatomic, retain) NSDate * createdTime;
 
 @end
--- instagram-photo-browser/Photo.m
+++ instagram-photo-browser/Photo.m
@@ -2,7 +2,7 @@
 //  Photo.m
 //  instagram-photo-browser
 //
-//  Created by Tyler Powers on 11/12/13.
+//  Created by Tyler Powers on 11/17/13.
 //  Copyright (c) 2013 Tyler Powers. All rights reserved.
 //
 
@@ -13,14 +13,12 @@
 
 @dynamic caption;
 @dynamic commentCount;
+@dynamic createdTime;
 @dynamic fullResImageURL;
 @dynamic identifier;
 @dynamic likeCount;
-@dynamic photoHeight;
-@dynamic photoWidth;
 @dynamic userFullName;
 @dynamic username;
 @dynamic userProfilePicURL;
-@dynamic createdTime;
 
 @end
